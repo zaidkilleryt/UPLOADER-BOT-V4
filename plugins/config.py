919e5cc22e51243ac292987cb906cfb0de12fc87
zaidkilleryt <123814688+zaidkilleryt@users.noreply.github.com>
2023-01-29 13:36:58 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,11 +11,11 @@ logging.basicConfig(
 
 class Config(object):
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6009374977:AAEIfV-kp9FhRwUUvH6JNNX1mY2FNn2MRlM")
 
-    API_ID = int(os.environ.get("API_ID", 23560088))
+    API_ID = int(os.environ.get("API_ID", 𝟮𝟴𝟰𝟰𝟭𝟭𝟭𝟴))
 
-    API_HASH = os.environ.get("API_HASH", "999c01704d5c417749a98f4b8785fe88")
+    API_HASH = os.environ.get("API_HASH", "𝟯𝗰𝟲𝟴𝗮𝟰𝗰𝗰𝟵𝟭𝟭𝗰𝗰𝟵𝟬𝗯𝗯𝗰𝗮𝗯𝗲𝟲𝗯𝟯𝟰𝟯𝟯𝟯𝟯𝟬𝗯𝟲")
 
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5410723702").split())
 
@@ -23,7 +23,7 @@ class Config(object):
 
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "sony_sab_all_shows_available")
 
     MAX_FILE_SIZE = 4194304000
 
@@ -43,21 +43,23 @@ class Config(object):
 
     PROCESS_MAX_TIMEOUT = 0
 
-    DEF_WATER_MARK_FILE = "Use this bot @UploadLinkToFileBot"
+    DEF_WATER_MARK_FILE = "Use this bot @jdissbh_bot"
 
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://zaidkilleryt:zaidali@007@cluster0.lwnjeyy.mongodb.net/?retryWrites=true&w=majority
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
+")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001642382009))
+    SESSION_NAME = os.environ.get("SESSION_NAME", "jdissbh_bot")
+
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001743813268))
 
     LOGGER = logging
 
-    OWNER_ID = int(os.environ.get("OWNER_ID", "5410723702"))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1405897472"))
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "sony_sab_all_shows_available")
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "UploadLinkToFileBot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "jdissbh_bot")
 
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
 
